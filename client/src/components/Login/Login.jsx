bd3eb8766706cc2d007e715ad845c4a4ec736bdb
Omar Loyo <qbytte@bits.local>
2023-02-13 15:21:12 -0600

    created login initial layout

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,7 +1,35 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useForm } from "react-hook-form";
+import { useLocation } from "wouter";
+import "./Login.css";
 
-export default Login
+const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const [, setLocation] = useLocation();
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit()}>
+        <input
+          type="text"
+          {...register("username")}
+          placeholder="Username"
+          required
+        />
+        <input
+          type="password"
+          {...register("password")}
+          required
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+      <p>
+        Don't have an account yet?{" "}
+        <a onClick={() => setLocation("/signup")}>Sign up</a>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
